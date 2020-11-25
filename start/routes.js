216da8f35f409ad7d2c385422888a2006baa31e8
kylanhurt <kylan.hurt@gmail.com>
2020-11-24 17:43:50 -0700

    Format routes file

--- start/routes.js
+++ start/routes.js
@@ -29,7 +29,10 @@ Route.get('/videos', 'VideoController.index')
 Route.get('/videos/rec/:quantity?/:username?', 'VideoController.getRecommended')
 Route.get('/videos/:id', 'VideoController.show')
 Route.get('/videos/processed/:id', 'VideoController.show') // may need to be changed later
-Route.get('/videos/recently-viewed/:username/:quantity?', 'VideoController.getRecentlyViewed')
+Route.get(
+	'/videos/recently-viewed/:username/:quantity?',
+	'VideoController.getRecentlyViewed'
+)
 
 // multimedia processing
 Route.post('/videos', 'MediaController.store').middleware('auth')
@@ -40,15 +43,25 @@ Route.post('/auth/login', 'AuthController.login')
 Route.get('/auth/check-username/:username', 'AuthController.checkUsername')
 Route.put('/auth/username', 'AuthController.updateUsername')
 Route.put('/user/profile', 'AuthController.saveProfile').middleware('auth')
-Route.post('/user/image/save', 'AuthController.saveProfileImage').middleware('auth')
-Route.post('/user/image/:type', 'AuthController.updateProfileImage').middleware('auth')
+Route.post('/user/image/save', 'AuthController.saveProfileImage').middleware(
+	'auth'
+)
+Route.post('/user/image/:type', 'AuthController.updateProfileImage').middleware(
+	'auth'
+)
 Route.get('/user/:field/:value', 'AuthController.getUserByParam')
 Route.get('/channel/:channel/:id?', 'AuthController.getChannel')
 
 // video ratings
 Route.post('/video-rating', 'VideoRatingController.store')
-Route.get('/video-rating/:uuid/user/:username', 'VideoRatingController.showUserRating')
+Route.get(
+	'/video-rating/:uuid/user/:username',
+	'VideoRatingController.showUserRating'
+)
 Route.get('/video-rating/:uuid', 'VideoRatingController.getVideoRatingStats')
 
 // user channel subscriptions
-Route.post('/subscription/:userId/:channel', 'UserChannelSubscriptionController.saveUserChannelSubscription')
+Route.post(
+	'/subscription/:userId/:channel',
+	'UserChannelSubscriptionController.saveUserChannelSubscription'
+)
